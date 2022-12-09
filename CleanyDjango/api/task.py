13f98ee0dbdf27271dfa11ab72125a4afc0ad2d5
Vadim Burtelov <v@burtelov.ru>
2022-12-10 01:11:57 +0300

    create task

---
+++ CleanyDjango/api/task.py
@@ -0,0 +1,8 @@
+from celery import shared_task
+from django.core.mail import send_mail
+import time
+
+@shared_task(serializer='json', name="send_mail")
+def send_email_fun(subject, message, sender, receiver):
+    time.sleep(20) # for check that sending email process runs in background
+    send_mail(subject, message, sender, [receiver])
