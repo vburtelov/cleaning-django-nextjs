1a0ded6ef2c69b00e72c1f4f3545b897495309c1
Vadim Burtelov <v@burtelov.ru>
2022-12-18 21:42:52 +0300

    add new constant

--- CleanyNextJS/src/constants.js
+++ CleanyNextJS/src/constants.js
@@ -1,3 +1,5 @@
-export const BASE_URL = process.env.NODE_ENV === "production"
-        ? process.env.NEXT_PUBLIC_BASE_URL_PROD
-        : process.env.NEXT_PUBLIC_BASE_URL_DEV;
+// export const BASE_URL = process.env.NODE_ENV === "production"
+//         ? process.env.NEXT_PUBLIC_BASE_URL_PROD
+//         : process.env.NEXT_PUBLIC_BASE_URL_DEV;
+export const BASE_URL = 'http://localhost:7777';
+export const SERVER_URL = 'http://django:8000';
