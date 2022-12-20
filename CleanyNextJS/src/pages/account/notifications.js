d0780358678342a80c9c1f275c642e943ed70c7d
Vadim Burtelov <v@burtelov.ru>
2022-12-20 16:46:37 +0300

    remove pages

--- CleanyNextJS/src/pages/account/notifications.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AccountNotifications from '../../components/Account/Notifications';
-
-const AccountNotificationsPage = () => {
-  return <AccountNotifications />;
-};
-
-export default AccountNotificationsPage;
