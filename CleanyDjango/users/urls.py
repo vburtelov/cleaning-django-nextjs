bee185bd0f0851d09c0d034a5e15c0ad0e3821bc
Vadim Burtelov <v@burtelov.ru>
2022-12-20 16:48:23 +0300

    set loginview for token url

--- CleanyDjango/users/urls.py
+++ CleanyDjango/users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView, TokenBlacklistView, TokenVerifyView,
 )
 
-from users.views import CustomUserViewSet, GetUserInformation, ChangePassword, CustomUserVerifyEmailView
+from users.views import CustomUserViewSet, GetUserInformation, ChangePassword, CustomUserVerifyEmailView, LoginView
 
 router = routers.DefaultRouter()
 router.register('', CustomUserViewSet, 'CustomUser')
@@ -14,7 +13,7 @@ urlpatterns = [
     path('me/', GetUserInformation.as_view(), name='me'),
     path('me/verify/', CustomUserVerifyEmailView.as_view(), name='verify_email'),
     path('me/changepass/', ChangePassword.as_view(), name='change_password'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', LoginView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
