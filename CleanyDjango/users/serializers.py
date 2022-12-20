6e0cd9152a3268960ee580fc19d3656259324a03
Vadim Burtelov <v@burtelov.ru>
2022-12-20 16:47:34 +0300

    add loginserializer for view is_active accounts

--- CleanyDjango/users/serializers.py
+++ CleanyDjango/users/serializers.py
@@ -1,7 +1,32 @@
 from django.contrib.auth.hashers import make_password
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from users.models import CustomUser, CustomGroup
+
+
+class LoginSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        authenticate_kwargs = {
+            self.username_field: attrs[self.username_field],
+            'password': attrs['password'],
+        }
+        try:
+            authenticate_kwargs['request'] = self.context['request']
+        except KeyError:
+            pass
+        try:
+            user = CustomUser.objects.get(email=authenticate_kwargs['email'])
+            if not user.check_password(authenticate_kwargs['password']):
+                self.error_messages['no_active_account'] = 'Неверный пароль'
+                raise exceptions.AuthenticationFailed(self.error_messages['no_active_account'], 'Неверный пароль')
+            if not user.is_active:
+                self.error_messages['no_active_account'] = 'Подтвердите почту'
+                raise exceptions.AuthenticationFailed(self.error_messages['no_active_account'], 'Подтвердите почту')
+        except CustomUser.DoesNotExist:
+            self.error_messages['no_active_account'] = 'Пользователь не найден'
+            raise exceptions.AuthenticationFailed(self.error_messages['no_active_account'], 'Пользователь не найден')
+        return super().validate(attrs)
 
 
 class CustomGroupSerializer(serializers.ModelSerializer):
