17ae01e30d4a1f1fae7775a626ee72c4f61dafd5
Vadim Burtelov <v@burtelov.ru>
2022-12-20 16:43:01 +0300

    remove billing

--- CleanyNextJS/src/pages/account/billing.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import AccountBilling from '../../components/Account/Billing';
-
-const AccountBillingPage = () => {
-
-  return <AccountBilling />;
-};
-
-export default AccountBillingPage;
