eaa6cdc2e3aa8b2c6afcdfac329417b130b4f8ab
Vadim Burtelov <v@burtelov.ru>
2022-12-20 16:46:27 +0300

    add domains for next/images && middleware for hot reload

--- CleanyNextJS/next.config.js
+++ CleanyNextJS/next.config.js
@@ -1,4 +1,14 @@
 // next.config.js
 module.exports = {
+    images: {
+          domains: ['assets.maccarianagency.com'],
+    },
+    webpackDevMiddleware: config => {
+        config.watchOptions = {
+            poll: 1000,
+            aggregateTimeout: 300,
+        }
+        return config
+    },
 
-};
+}
