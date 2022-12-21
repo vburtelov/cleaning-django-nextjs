1096a4e60edd09db73b26db57af87a1ec9d99447
Vadim Burtelov <v@burtelov.ru>
2022-12-21 07:58:30 +0300

    add 2 new reducers

--- CleanyNextJS/src/store.js
+++ CleanyNextJS/src/store.js
@@ -2,10 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice'
 import cleanerReducer from './slices/cleanerSlice'
 import orderReducer from './slices/orderSlice'
+import promoReducer from './slices/promoSlice'
+import typeReducer from './slices/typeSlice'
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     cleaner: cleanerReducer,
-    order: orderReducer
+    order: orderReducer,
+    promo: promoReducer,
+    types: typeReducer,
   },
 })
